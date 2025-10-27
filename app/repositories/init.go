4b7282bcfeec6228e6ce18aab00389ee51bf4531
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2025-10-27 11:55:41 +0700

    Add swagger, and setup other things

--- app/repositories/init.go
+++ app/repositories/init.go
@@ -1,1 +1,17 @@
 package repositories
+
+import (
+	"app/domain"
+
+	"gorm.io/gorm"
+)
+
+type appRepository struct {
+	Conn *gorm.DB
+}
+
+func NewAppRepository(conn *gorm.DB) domain.AppRepository {
+	return &appRepository{
+		Conn: conn,
+	}
+}
---
+++ domain/repository.go
@@ -0,0 +1,4 @@
+package domain
+
+type AppRepository interface {
+}
