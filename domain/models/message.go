46d3be6f6c7e63543d83303c9f829551d1a1adbf
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2025-11-26 16:35:43 +0700

    Refactor models to enhance foreign key relationships and indexing; update migration logic to drop existing tables before auto-migration

--- domain/models/message.go
+++ domain/models/message.go
@@ -3,11 +3,14 @@ package models
 import "time"
 
 type Message struct {
-	ID             uint64     `json:"id" gorm:"primaryKey"`
+	ID             uint64     `json:"id" gorm:"primaryKey;"`
 	ConversationID uint64     `json:"conversation_id" gorm:"not null;index"`
-	SenderID       uint64     `json:"sender_id" gorm:"not null"`
+	SenderID       uint64     `json:"sender_id" gorm:"not null;index"`
 	MessageText    string     `json:"message_text" gorm:"type:text"`
 	DeletedAt      *time.Time `json:"deleted_at,omitempty" gorm:"index"`
 	PurgeAt        *time.Time `json:"purge_at,omitempty" gorm:"index"`
 	CreatedAt      time.Time  `json:"created_at" gorm:"autoCreateTime"`
+
+	Conversation Conversation `json:"conversation" gorm:"foreignKey:ConversationID"`
+	Sender       User         `json:"sender" gorm:"foreignKey:SenderID"`
 }
--- domain/models/user.go
+++ domain/models/user.go
@@ -3,12 +3,15 @@ package models
 import "time"
 
 type User struct {
-	ID        uint64    `json:"id" gorm:"primaryKey;autoIncrement:false"`
+	ID        uint64    `json:"id" gorm:"primaryKey;"`
 	Username  string    `json:"username" gorm:"unique;not null"`
 	Email     string    `json:"email" gorm:"unique;not null"`
 	Role      UserRole  `json:"role" gorm:"default:'customer'"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+
+	Conversations []Conversation `json:"conversations" gorm:"foreignKey:CustomerID"`
+	Messages      []Message      `json:"messages" gorm:"foreignKey:SenderID"`
 }
 
 type UserRole string
